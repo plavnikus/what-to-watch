83af880aa20a8ad6f6eb6eb8f70c635c2bc227c3
plavnikus <stroy38.ru@gmail.com>
2026-09-06 20:49:55 +0800

    Block obsolete VentKub payments route

---
+++ functions/ventkub-payments/_middleware.js
@@ -0,0 +1,10 @@
+export async function onRequest() {
+  return new Response('Not Found', {
+    status: 404,
+    headers: {
+      'Content-Type': 'text/plain; charset=utf-8',
+      'Cache-Control': 'no-store',
+      'X-Robots-Tag': 'noindex, nofollow, noarchive'
+    }
+  });
+}
